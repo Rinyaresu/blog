582f4402b90ac4ce3b821ae3707d5fabc418b791
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-16 23:01:35 -0300

    feat(app): blog

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://rinyaresu.dev",
+  integrations: [mdx(), sitemap()],
+  output: "static",
+  adapter: vercel(),
+});
---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  title: "Rinyaresu's garden",
+  website: "https://rinyaresu.dev",
+  description: "A place where I put all my thoughts",
+  author: "Kaique Linhares <aka Rinyaresu>",
+};
+
+export const GITHUB_REPO_URL = "https://github.com/rinyaresu/blog";
+export const TWITTER_URL = "https://twitter.com/rinyaresu";
